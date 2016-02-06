a52564f7ff1b39f5adb3d5e6850dc018b8f86618
Zander van der Meer <zander.meer@gmail.com>
2016-02-06 10:48:09 +0100

    Updated Composer file

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,18 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix.styles([
+        'bootstrap.css',
+        'sb-admin.css',
+        'plugins/morris.css',
+        'plugins/font-awesome.css'
+    ], 'public/assets/css/style.css');
+
+    mix.scripts([
+        'jquery.js',
+        'bootstrap.js',
+        'plugins/morris/raphael.min.js',
+        'plugins/morris/morris.min.js',
+        'plugins/morris/morris-data.js'
+    ], 'public/assets/js/javascript.js');
 });
