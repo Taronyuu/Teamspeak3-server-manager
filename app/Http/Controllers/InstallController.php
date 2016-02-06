b45dc194509eea56c79c62966d3705e9ac5b5f72
Zander van der Meer <zander.meer@gmail.com>
2016-02-06 15:10:12 +0100

    Added an installation page. This one is only available when there are no users yet.

---
+++ app/Http/Controllers/InstallController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+
+class InstallController extends Controller
+{
+
+    public function __construct(){
+        if(User::count()){
+            return redirect()->action('DashboardController@index')->with('error', 'There is already an existing user. Can\'t reinitate install');
+        }
+    }
+
+    public function index()
+    {
+        return view('install');
+    }
+
+    public function install(Requests\PostInstallationRequest $request)
+    {
+        $data = $request->all();
+        $data['password'] = Hash::make($data['password']);
+        $user = User::create($data);
+
+        return redirect()->action('DashboardController@index')->with('success', 'Installation has been completed');
+    }
+}
