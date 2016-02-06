5c5440c2691f223ef4c6f6f9a4a4c7ad23ad6c34
Zander van der Meer <zander.meer@gmail.com>
2016-02-06 14:36:49 +0100

    Added Teamspeak viewer

---
+++ resources/views/pages/servers/configure.blade.php
@@ -0,0 +1,32 @@
+@extends('app')
+
+@section('page_title', 'Create server')
+
+@section('content')
+    {!! Form::open(['action' => 'ServerController@store', 'method' => 'post', 'role' => 'form']) !!}
+    <div class="row">
+        @include('partials.messages')
+        <div class="col-md-6">
+            <div class="form-group">
+                {!! Form::label('name', 'Server name') !!}
+                {!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Server name...', 'required' => 'true']) !!}
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="form-group">
+                {!! Form::label('slots', 'Slots') !!}
+                {!! Form::number('slots', null, ['class' => 'form-control', 'placeholder' => 4, 'steps' => 1]) !!}
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="form-group">
+                {!! Form::submit('Create', ['class' => 'btn btn-primary', 'style' => 'width: 100%']) !!}
+            </div>
+        </div>
+    </div>
+    {!! Form::close() !!}
+@endsection
