40a3aeec7e5f38b85461d04138b99dac16724c36
Zander van der Meer <zander.meer@gmail.com>
2016-02-06 14:38:43 +0100

    Added console command to reset a server. (Remove all existing) This is needed because data should be stored in the sqlite DB. Next step is a sync command

---
+++ app/Console/Commands/ResetTeamspeakServer.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Helpers\TeamspeakHelper;
+use App\Models\Server;
+use Illuminate\Console\Command;
+
+class ResetTeamspeakServer extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'teamspeak:reset';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '[DANGEROUS]Removes all existing Teamspeak server in your instance';
+
+    protected $teamspeak;
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct(TeamspeakHelper $teamspeak)
+    {
+        parent::__construct();
+        $this->teamspeak = $teamspeak;
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+//        $serverInstance = $this->teamspeak->getInstance();
+//        foreach($serverInstance as $virtualServer){
+//            $serverInstance->serverStop([
+//                'sid'   => $virtualServer['virtualserver_id'],
+//            ]);
+//            $serverInstance->serverDelete([
+//                'sid'   => $virtualServer['virtualserver_id'],
+//            ]);
+//        }
+
+        $servers = Server::truncate();
+    }
+}
