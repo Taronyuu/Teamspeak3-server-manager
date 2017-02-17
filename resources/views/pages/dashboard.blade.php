237f730213df7f1b0ade41d17c02ebb2addc9dfc
Dennis Smink <dennissmink@gmail.com>
2017-02-17 12:10:57 +0100

    Fixed url for main button @ dashboard

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -18,7 +18,7 @@
                         </div>
                     </div>
                 </div>
-                <a href="#">
+                <a href="{{ action('ServerController@index') }}">
                     <div class="panel-footer">
                         <span class="pull-left">View</span>
                         <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
